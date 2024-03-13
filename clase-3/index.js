6f1f234fc1f7bc2b64a6c3b80bca08936922d3d5
Jose Villacorta <villacortafur@gmail.com>
2024-03-13 01:53:56 -0500

    Tema de POO y Interface

--- clase-3/index.js
+++ clase-3/index.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 console.log("Hola mundo desde Typescript");
 var num = 12;
 var flag = false;
@@ -44,3 +59,83 @@ console.log(calcularImpuesto(100, 0.19));
 console.log(calcularImpuesto(100));
 var fn = new Function("a", "b", "return a*b");
 console.log(fn(5, 4));
+//Programacion Orienta a Objetos
+var book = /** @class */ (function () {
+    function book(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.date_published = "12/03/2024";
+    }
+    book.prototype.shouwDuebt = function () {
+        console.log("shouwDuebt");
+    };
+    book.prototype.feedback = function () {
+        console.log("feedback");
+    };
+    book.prototype.setTitle = function (title) {
+        this.title = title;
+    };
+    return book;
+}());
+var b1 = new book("Mi planta de naranja", "Jose Vasconcelos", "ISBN-123456789");
+b1.isbn = "ISBN-123456789xyz";
+b1.setTitle("Mi planta de Marihuana");
+console.log(b1);
+//Herencia
+var Shape = /** @class */ (function () {
+    function Shape(a) {
+        this.area = a;
+    }
+    return Shape;
+}());
+var Circulo = /** @class */ (function (_super) {
+    __extends(Circulo, _super);
+    function Circulo(r, a) {
+        if (a === void 0) { a = 0; }
+        var _this = _super.call(this, a) || this;
+        _this.radio = r;
+        return _this;
+    }
+    Circulo.prototype.calcularArea = function () {
+        this.area = (3.1416 * this.radio * this.radio) / 2;
+    };
+    return Circulo;
+}(Shape));
+var circulo = new Circulo(5);
+console.log(circulo);
+circulo.calcularArea();
+console.log(circulo);
+var cliente = {
+    nombre: "Jose",
+    apellido: "Villacorta",
+    saludar: function () {
+        return "Hola " + this.nombre + " " + this.apellido;
+    }
+};
+console.log(cliente.saludar());
+cliente.nombre = "Fernando";
+console.log(cliente.saludar());
+var Usuario = /** @class */ (function () {
+    function Usuario(nombre, apellido, sexo, edad) {
+        if (edad === void 0) { edad = 0; }
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.sexo = sexo;
+    }
+    Usuario.prototype.saludar = function () {
+        return "Hi " + this.apellido;
+    };
+    return Usuario;
+}());
+var user = new Usuario("Jorge", "Becerra", "Masculino", 16);
+console.log(user);
+console.log(user.saludar());
+function sumarPuntos(p1, p2) {
+    var x = p1.x + p2.x;
+    var y = p1.y + p2.y;
+    return { x: x, y: y };
+}
+var p1 = { x: 0, y: 10 };
+var p2 = { x: 5, y: -3 };
+console.log(sumarPuntos(p1, p2));
--- clase-3/index.ts
+++ clase-3/index.ts
@@ -56,3 +56,115 @@ var fn = new Function("a", "b", "return a*b")
 
 console.log(fn(5,4));
 
+//Programacion Orienta a Objetos
+class book {
+    private title: string;
+    private author: string;
+    public isbn: string;
+    public date_published: string;
+
+    constructor(title:string, author:string, isbn:string){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.date_published = "12/03/2024";
+    }
+
+    shouwDuebt(): void{
+        console.log("shouwDuebt");
+    }
+
+    feedback(): void{
+        console.log("feedback");
+    }
+
+    setTitle(title:string):void{
+        this.title = title;
+    }
+    
+}
+
+var b1 = new book("Mi planta de naranja", "Jose Vasconcelos", "ISBN-123456789");
+b1.isbn = "ISBN-123456789xyz"
+b1.setTitle("Mi planta de Marihuana");
+console.log(b1);
+
+//Herencia
+class Shape{
+    area: number;
+
+    constructor(a:number){
+        this.area = a;
+    }
+}
+
+class Circulo extends Shape {
+    radio: number;
+
+    constructor(r:number, a:number=0){
+        super(a);
+        this.radio = r;
+    }
+
+    calcularArea():void{
+        this.area = (3.1416 * this.radio * this.radio) / 2;
+    }
+}
+
+var circulo = new Circulo(5);
+console.log(circulo);
+circulo.calcularArea();
+console.log(circulo);
+
+//Interface
+interface IPersona{
+    nombre:string;
+    apellido:string;
+    saludar: ()=>string;
+}
+
+var cliente:IPersona = {
+    nombre: "Jose",
+    apellido: "Villacorta",
+    saludar:function() {
+        return "Hola "+this.nombre+" "+this.apellido;
+    }
+}
+
+console.log(cliente.saludar());
+cliente.nombre = "Fernando";
+console.log(cliente.saludar());
+
+class Usuario implements IPersona{
+    nombre:string;
+    apellido:string;
+    sexo:string;
+    edad:number
+
+    constructor(nombre:string, apellido:string, sexo:string, edad:number = 0){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.sexo = sexo;
+    }
+
+    saludar():string{
+        return "Hi "+this.apellido;
+    }
+}
+var user = new Usuario("Jorge", "Becerra", "Masculino", 16);
+console.log(user);
+console.log(user.saludar());
+
+interface IPunto2D{
+    x:number;
+    y:number;
+}
+function sumarPuntos(p1:IPunto2D, p2:IPunto2D){
+    let x = p1.x + p2.x;
+    let y = p1.y + p2.y;
+    return{x:x, y:y};
+}
+
+var p1 = {x:0, y:10};
+var p2 = {x:5, y:-3};
+console.log(sumarPuntos(p1, p2));
